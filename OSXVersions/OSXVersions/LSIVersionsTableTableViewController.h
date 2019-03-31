116c55dc52bc2604871372ed3f225c3464b0bcf4
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-30 21:44:54 -0400

    add LSI file and set table view controller's class to it

---
+++ OSXVersions/OSXVersions/LSIVersionsTableTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LSIVersionsTableTableViewController.h
+//  OSXVersions
+//
+//  Created by Julian A. Fordyce on 3/30/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIVersionsTableTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
